a5b005c55beb272273e6211245b4cc4d24d14e74
Emperor <Emperor@DESKTOP-EAPO0E9>
2016-02-21 14:42:06 +0200

    Module 1

---
+++ src/main/java/Anagram/ReverseStringApp.java
@@ -0,0 +1,36 @@
+package Anagram;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ReverseStringApp {
+
+	public static void main(String[] args) {
+		System.out.println("Enter String");
+		Scanner sc = new Scanner(System.in);
+		String inputString = sc.nextLine();
+		sc.close();
+		char[] arr = inputString.toCharArray();
+		String tempWord ="";
+		for (int charIndex = 0 ; charIndex < arr.length ; charIndex++){
+			if(' '==arr[charIndex] ){
+			String toOut = new StringBuffer(tempWord).reverse().toString();
+			tempWord = "";
+			System.out.print(toOut + " ");
+				
+			}else{
+			tempWord = tempWord + arr[charIndex];
+			}if(charIndex+1 == arr.length){
+				String toOut = new StringBuffer(tempWord).reverse().toString();
+				System.out.print(toOut + " ");
+			}
+			
+		}
+		
+		
+	}
+
+	
+	}
+
---
+++ src/main/java/MergeSort/MergeSort.java
@@ -0,0 +1,55 @@
+package MergeSort;
+
+import java.util.Arrays;
+
+public class MergeSort {
+	public static void main(String[] args) {
+
+		Integer[] a = { 2, 6, 3, 5, 1, 7, 8, 4 , 9 ,15 ,65 };
+
+		mergeSort(a);
+
+		System.out.println(Arrays.toString(a));
+	}
+
+	public static Comparable[] mergeSort(Comparable[] list) {
+
+		if (list.length <= 1) {
+			return list;
+		}
+
+		Comparable[] first = new Comparable[list.length / 2];
+		Comparable[] second = new Comparable[list.length - first.length];
+		System.arraycopy(list, 0, first, 0, first.length);
+		System.arraycopy(list, first.length, second, 0, second.length);
+
+		mergeSort(first);
+		mergeSort(second);
+
+		merge(first, second, list);
+		return list;
+	}
+
+	private static void merge(Comparable[] first, Comparable[] second, Comparable[] result) {
+
+		int iFirst = 0;
+
+		int iSecond = 0;
+
+		int iMerged = 0;
+
+		while (iFirst < first.length && iSecond < second.length) {
+			if (first[iFirst].compareTo(second[iSecond]) < 0) {
+				result[iMerged] = first[iFirst];
+				iFirst++;
+			} else {
+				result[iMerged] = second[iSecond];
+				iSecond++;
+			}
+			iMerged++;
+		}
+
+		System.arraycopy(first, iFirst, result, iMerged, first.length - iFirst);
+		System.arraycopy(second, iSecond, result, iMerged, second.length - iSecond);
+	}
+}
